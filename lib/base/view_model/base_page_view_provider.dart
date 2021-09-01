9c1d9a0d5ff7c7baca31da98d842f328e417a4f2
Rinsan <cyjdtxz@gmail.com>
2021-09-01 23:00:59 +0800

    feat: 调整 BasePageProvider

--- lib/base/view_model/base_page_view_provider.dart
+++ lib/base/view_model/base_page_view_provider.dart
@@ -4,5 +4,20 @@
 /// created by DZDcyj at 2021/8/29
 ///
 import 'package:flutter/cupertino.dart';
+import 'package:rxdart/rxdart.dart';
 
-abstract class BasePageProvider extends ChangeNotifier {}
+abstract class BasePageProvider extends ChangeNotifier {
+  bool isDisposed = false;
+
+  CompositeSubscription _compositeSubscription = CompositeSubscription();
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (isDisposed) {
+      return;
+    }
+    isDisposed = true;
+    _compositeSubscription.dispose();
+  }
+}
